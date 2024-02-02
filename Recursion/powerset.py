7f695d60cec7c4fff85dc0e13ab44d564eb4bf26
vikas6050 <vikas6050@gmail.com>
2024-02-02 11:38:56 +0530

    recursion1

---
+++ Recursion/powerset.py
@@ -0,0 +1,15 @@
+def power_set(nums):
+    output = []
+    def helper (nums,i,subset):
+        if i == len(nums):
+            output.append(subset.copy())
+            return
+        helper(nums,i+1,subset)
+        subset.append(nums[i])
+        helper(nums,i+1,subset)
+        subset.pop()
+    helper(nums,0,[])
+    return output 
+
+
+print(power_set([1,8,7]))
